3d37727e6c89042491e52558d1cc896c2fcc1698
hideakifukami <denys.fukami@gmail.com>
2024-02-28 13:43:11 -0300

    daysofweek

--- aula02/src/DaysOfWeek/DiaDaSemana.java
+++ aula02/src/DaysOfWeek/DiaDaSemana.java
@@ -6,31 +6,31 @@ public class DiaDaSemana {
 	
 	public void verificarDia(int dia) {
 		switch (dia) {
-		case 0:
+		case 1:
 			strategy = new Domingo();
 			break;
 			
-		case 1:
+		case 2:
 			strategy = new Segunda();
 			break;
 
-		case 2:
+		case 3:
 			strategy = new Terca();
 			break;
 
-		case 3:
+		case 4:
 			strategy = new Quarta();
 			break;
 
-		case 4:
+		case 5:
 			strategy = new Quinta();
 			break;
 
-		case 5:
+		case 6:
 			strategy = new Sexta();
 			break;
 
-		case 6:
+		case 7:
 			strategy = new Sabado();
 			break;
 
--- aula02/src/DaysOfWeek/Main.java
+++ aula02/src/DaysOfWeek/Main.java
@@ -1,23 +1,16 @@
 package DaysOfWeek;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Main {
 
 	public static void main(String[] args) {
 		DiaDaSemana dds = new DiaDaSemana();
-		Date date = new Date();
-		GregorianCalendar dataCorrente = new GregorianCalendar(2024, 2, 23);
-		
-		System.out.println(dataCorrente.get(Calendar.DAY_OF_WEEK));
-
+		GregorianCalendar dataCorrente = new GregorianCalendar();
+				
 		dds.verificarDia(dataCorrente.get(Calendar.DAY_OF_WEEK));
-		
 		dds.exibirMensagem();
-		
-		System.out.println(Calendar.DAY_OF_WEEK);
 		
 	}
 
